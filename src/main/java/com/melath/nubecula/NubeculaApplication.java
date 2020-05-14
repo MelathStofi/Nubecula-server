b7fc30de4b020fdc1e2b7be7c589bc9233488dc8
MelathStofi <melath.stofi@gmail.com>
2020-05-15 01:37:31 +0200

    initialized

---
+++ src/main/java/com/melath/nubecula/NubeculaApplication.java
@@ -0,0 +1,13 @@
+package com.melath.nubecula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NubeculaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NubeculaApplication.class, args);
+    }
+
+}
